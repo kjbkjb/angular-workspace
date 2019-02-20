ba810f4f49c8759714a529fd0d16469b4032b145
Kevin Boedigheimer <kevin.boedigheimer@sportngin.com>
2019-02-20 08:46:12 -0600

    add some html and events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PagingService} from './pagination/paging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-workspace';
+  constructor(private pagingService: PagingService){}
+
+  ngOnInit(): void {
+    this.pagingService.collect({current_page: 1, first_page: true, last_page: false, limit: 2, offset: 4, total: 6, total_pages: 10});
+  }
 }
